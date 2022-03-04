b7dec9354599e8e0e7b3b93d80fa702f829a026d
login <matvsuzu@gmail.com>
2022-03-04 23:20:09 +0300

    gitignore + delete 1 useless row

--- GeekCourses/lesson_six/ex_five.py
+++ GeekCourses/lesson_six/ex_five.py
@@ -19,7 +19,6 @@ hobby_f.seek(0)
 
 with open(general, 'w', encoding='UTF-8') as general_f:
     for line_user, line_hobby in zip_longest(user_f, hobby_f):  # Мы используем цикл for вместо того, чтобы целиком открыть файл
-        print(line_user.strip().split(','), line_hobby.strip().split(',') if line_hobby is not None else line_hobby)
         general_f.write(f'{line_user.strip().split(",")}: '
                         f'{line_hobby.strip().split(",") if line_hobby is not None else line_hobby}\n')
 
