3ef61a736786863f765f69de9478c54cb85c2ae6
Matvey Syusyukin <matvey.syusyukin@dnk.ru>
2022-03-29 00:05:05 +0300

    ex_one, ex_two

---
+++ GeekCourses/lesson_nine/ex_one.py
@@ -0,0 +1,12 @@
+class TrafficLight:
+    def __init__(self):
+        self.__color = (('Red', 5), ('Yellow', 2), ('Green', 4))  # атрибут реализовать как приватный;
+
+    def running(self):
+        while True:
+            for color, sec in self.__color:
+                print(color, f'(wait {sec} sec)')
+
+
+light = TrafficLight()
+light.running()
---
+++ GeekCourses/lesson_nine/ex_three.py
@@ -0,0 +1,3 @@
+class Worker:
+    def __init__(self, name, surname, position, income):
+        self.__income = 
